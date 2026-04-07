23abea340dd23337a0f7c8a13a9dbe4be4d0b43c
ciepmaroc-ops <ciepmaroc@gmail.com>
2026-04-07 14:07:26 +0100

    Update assets.service.ts

--- src/modules/assets/assets.service.ts
+++ src/modules/assets/assets.service.ts
@@ -1,4 +1,4 @@
-import { env } from '../../config/env';
+import { env } from '../../Config/env';
 import { logger } from '../../utils/logger';
 import { AppError } from '../../middleware/errorHandler';
 
